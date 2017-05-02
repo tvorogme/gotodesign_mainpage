4e4907621a7d742ae7a47a23ca655a0cfba2bd49
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-05-02 09:46:18 +0300

    Add dump of matrix

--- js/tetris/tetris.js
+++ js/tetris/tetris.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     var c_tetris = document.getElementById("tetris");
     var tetris_context = c_tetris.getContext("2d");
 
-    tetris_context.width = 900;
-    tetris_context.height = 400;
+    c_tetris.width = 900;
+    c_tetris.height = 400;
 
     var game_field = [];
+    var game_freezed = [];
 
-    var rows = 7;
-    var cols = 9;
+    var rows = 8;
+    var cols = 18;
 
     var step_time = 300;
 
@@ -20,6 +21,7 @@ $(document).ready(function () {
         }
 
         game_field.push(tmp);
+        game_freezed.push(tmp);
     }
 
     var shapes = [
@@ -79,28 +81,18 @@ $(document).ready(function () {
             var tmp = [];
 
             for (var g = 0; g < cols; g++) {
-                tmp.push(0);
+                tmp.push(game_freezed[i][g] | 0);
             }
 
             game_field.push(tmp);
         }
+
     }
 
     clear_game();
 
     var cur_X, cur_Y = 0;
 
-    function validate(X, Y, block) {
-        return !(game_field[Y + 1][X] === 1 && block[2][X] === 1 ||
-        game_field[Y + 1][X + 1] === 1 && block[2][X + 1] === 1 ||
-        game_field[Y + 1][X + 2] === 1 && block[2][X + 2] === 1 ||
-        game_field[Y + 1][X + 3] === 1 && block[2][X + 3] === 1 ||
-
-        game_field[Y][X] === 1 && block[1][X] === 1 ||
-        game_field[Y][X + 1] === 1 && block[1][X + 1] === 1 ||
-        game_field[Y][X + 2] === 1 && block[1][X + 2] === 1 ||
-        game_field[Y][X + 3] === 1 && block[1][X + 3] === 1)
-    }
 
     function valid(offsetX, offsetY, X, Y) {
         offsetX = offsetX || 0;
@@ -108,11 +100,11 @@ $(document).ready(function () {
         offsetX = X + offsetX;
         offsetY = Y + offsetY;
 
-        for (var y = 0; y < 4; ++y) {
-            for (var x = 0; x < 4; ++x) {
+        for (var y = 0; y < 2; ++y) {
+            for (var x = 0; x < 2; ++x) {
                 if (typeof game_field[y + offsetY] == 'undefined'
                     || typeof game_field[y + offsetY][x + offsetX] == 'undefined'
-                    || game_field[y + offsetY][x + offsetX]
+                    || game_field[offsetY][offsetX]
                     || x + offsetX < 0
                     || y + offsetY >= rows
                     || x + offsetX >= cols) {
@@ -124,6 +116,7 @@ $(document).ready(function () {
         return true;
     }
 
+    var lock_tetris = false;
 
     function step(block, position) {
         cur_X = position;
@@ -138,9 +131,24 @@ $(document).ready(function () {
             n = 0;
         }
 
-
+        lock_tetris = true;
         var game_interval = window.setInterval(function () {
-            if (cur_X + 4 >= game_field[0].length || cur_Y + 2 >= game_field.length || valid()) {
+            var offsetY = 1;
+
+            if (1 in block[1]) {
+                offsetY = 2;
+            }
+
+            var offsetX = 0;
+
+            for (var k = 0; k < block[1].length; k += 1) {
+                if (block[1][k] === 1) {
+                    offsetX = k;
+                }
+            }
+
+            if (!valid(offsetX, offsetY, cur_X, cur_Y)) {
+                lock_tetris = false;
                 window.clearInterval(game_interval);
             }
 
@@ -150,21 +158,32 @@ $(document).ready(function () {
             var h = 0;
             for (g = 0; g < 2; g++) {
                 for (i = 0; i < 4; i++) {
-                    game_field[cur_Y + h][cur_X + n] = block[g][i];
+                    game_field[cur_Y + h][cur_X + n] += block[g][i];
                     n += 1
                 }
                 h += 1;
                 n = 0;
             }
-            validate(cur_X, cur_Y, block);
-        }, 1000);
+
+            if (!lock_tetris){
+                game_freezed = game_field;
+            }
+
+
+        }, 300);
 
     }
 
-    step(qubes[0], positions[0]);
+    step(qubes[3], positions[0]);
+
+    setInterval(function () {
+        if (lock_tetris === false) {
+            step(qubes[1], positions[0]);
+        }
+    }, 1000);
 
     var W = 900, H = 400;
-    var BLOCK_W = W / 30, BLOCK_H =(H / 18) + 2;
+    var BLOCK_W = W / cols, BLOCK_H = H / rows;
 
     function drawBlock(x, y) {
         tetris_context.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
@@ -173,7 +192,6 @@ $(document).ready(function () {
 
     function render() {
         tetris_context.clearRect(0, 0, W, H);
-
         tetris_context.strokeStyle = 'black';
         for (var x = 0; x < cols; ++x) {
             for (var y = 0; y < rows; ++y) {
